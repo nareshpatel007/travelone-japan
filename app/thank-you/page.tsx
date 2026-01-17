1d0237f422b78c77b112ae86c22e885eb5a7d43d
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2026-01-17 19:13:18 +0530

    Update page.tsx

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 import CommonHeader from "@/components/header/common-header";
-import CommonMobileHeader from "@/components/header/common-mobile-header";
 import CommonFooter from "@/components/footer/common-footer";
-import CommonTopHeader from "@/components/header/common-top-header";
-import IncludeJs from "@/components/includes/common-js";
-import IncludeCss from "@/components/includes/css";
-import Script from "next/script";
 
 export default function ContactUsPage() {
     return (
         <body className="wp-singular page-template page-template-page-full-width page-template-page-full-width-php page page-id-280 wp-theme-wanderaway theme-wanderaway qi-blocks-1.4.3 qodef-gutenberg--no-touch qode-framework-1.2.6 woocommerce-js qodef-qi--no-touch qi-addons-for-elementor-1.9.3 wanderaway-core-1.2 wanderaway-1.1.1 qodef-content-grid-1300 qodef-back-to-top--enabled qodef-header--standard qodef-header-appearance--sticky qodef-mobile-header--side-area qodef-drop-down-second--full-width qodef-drop-down-second--default qode-export-1.0 qodef-header-standard--center qodef-search--covers-header elementor-default elementor-kit-4 elementor-page elementor-page-280 qodef-browser--chrome e--ua-blink e--ua-chrome e--ua-webkit">
-            <CommonTopHeader />
             <CommonHeader />
-            <CommonMobileHeader />
 
             <section className="!pb-10 !mb:pb-20 bg-white">
                 <div className="!max-w-7xl !mx-auto !px-8">
