2a210edcb3c9c259d73afab75c8e53281fc87703
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2026-01-20 18:11:48 +0530

    Update home-mobile-header.tsx

--- components/header/home-mobile-header.tsx
+++ components/header/home-mobile-header.tsx
@@ -63,12 +63,9 @@ export default function HomeMobileHeader() {
 
                     <nav className="flex flex-col gap-6 p-6 text-lg font-medium">
                         <Link href="/">Home</Link>
-                        <Link href="/destination">Destination</Link>
-                        <Link href="/tour">Tours</Link>
-                        <Link href="/about">About Us</Link>
-                        <Link href="/contact">Contact Us</Link>
-                        <Link href="/blog">Blog</Link>
-                        <Link href="/booking">Booking</Link>
+                        <Link href="/country">Destination</Link>
+                        <Link href="/about">About</Link>
+                        <Link href="/contact">Contact</Link>
                     </nav>
                 </div>
             )}
