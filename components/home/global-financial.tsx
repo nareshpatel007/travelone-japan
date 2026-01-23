614bfd386e068a11ab65a2834115fe4703d4ae3c
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2026-01-23 11:05:51 +0530

    Update global-financial.tsx

--- components/home/global-financial.tsx
+++ components/home/global-financial.tsx
@@ -15,7 +15,18 @@ export default function GlobalFinancialSection() {
                             Travel with the confidence of North American financial protection and MoR.
                         </span>
                     </div>
+
                     <p className="text-black text-base sm:text-lg">Travel with the confidence of North American financial protection. TravelOne USA acts as your Merchant of Record. We assume the liability, we manage the global taxes, and we guarantee your refunds. You simply explore.</p>
+
+                    <Image
+                        src="https://ik.imagekit.io/288weifiq/nextjs/81-814150_acta-logo-clipart.png"
+                        alt="acta logo"
+                        width={200}
+                        height={200}
+                        className="object-cover transition-transform duration-700 hover:scale-105"
+                    />
+
+                    <p className="text-black text-base sm:text-lg">Our global operations are led by certified members of the industry's leading professional associations.</p>
                 </div>
             </div>
             <div className="flex items-center justify-center">
