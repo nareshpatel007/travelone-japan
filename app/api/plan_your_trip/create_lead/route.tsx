154958b13f4a88f0629bb1547dba2c0ac1ac7624
Naresh Patel <30832955+nareshpatel007@users.noreply.github.com>
2026-02-02 15:35:40 +0530

    Update route.tsx

--- app/api/plan_your_trip/create_lead/route.tsx
+++ app/api/plan_your_trip/create_lead/route.tsx
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
 
     // Make API request
     const body = await req.json().catch(() => ({}));
-    const res = await fetch("${process.env.API_URL}plan_your_trip/create_lead", {
+    const res = await fetch(`${process.env.API_URL}plan_your_trip/create_lead`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
